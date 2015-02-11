c46eafa2a45d3d20a33e2b86b93024392c84104d
John V. <crazzy.john@gmail.com>
2015-02-11 16:38:01 -0600

    Fixing more git exclude nonsense plus
    Also Updated Gameroot with some more organization ITS NOT COMPLETE YET
    but give it a look if you like.

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ LIBRARY_PATHS = -Lmingw_dev_lib\lib
 #COMPILER_FLAGS specifies the additional compilation options we're using
 # -w suppresses all warnings
 # -Wl,-subsystem,windows gets rid of the console window
-COMPILER_FLAGS = -w -Wl,-subsystem,windows
+#COMPILER_FLAGS = -w -Wl,-subsystem,windows
 
 #LINKER_FLAGS specifies the libraries we're linking against
 LINKER_FLAGS = -lmingw32 -lSDL2main -lSDL2
--- gameroot.cpp
+++ gameroot.cpp
@@ -1,77 +1,106 @@
 //INCLUDES
 #include "SDL.h"
 #include <stdio.h>
+#include <string>
 
-void initialize()
+using namespace std;
+
+
+//Global Variables
+SDL_Window *window = NULL;
+SDL_Surface *surface = NULL;
+SDL_Surface *image = NULL;
+//String window_name = "Rogue Engine Window Title Here"
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 720;
+bool initialize()
 {
 
+	bool success = true;
+
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+
+		printf("SDL could not initialize! SDL_ERROR: %s\n", SDL_GetError() );
+		success = false;
+	}
+
+
+	window = SDL_CreateWindow( "Rogue Engine Window Title Here", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+    // Check that the window was successfully made
+    if (window == NULL) {
+        // In the event that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        success = false;
+    }
+    else
+    {
+    	//Window Surface
+    	surface = SDL_GetWindowSurface(window);
+    	SDL_FillRect( surface, NULL, SDL_MapRGBA(surface->format, 55,55,55,55));
+    }
+
+return success;
+
+}
+
+bool loadMedia()
+{
+	bool success = true;
+
+	image = SDL_LoadBMP ("rogue-engine/dino.bmp");
+	if(image == NULL)
+	{
+		printf ("The image didn't load. Error: %s", SDL_GetError() );
+		success = false;
+	}
+
+
+	return success;
 
 }
 
 void update()
 {
 
+	//SDL_BlitSurface( image, NULL, surface, NULL);
 
+	SDL_FillRect( surface, NULL, SDL_MapRGBA(surface->format, 155,155,155,155));
 }
 
 void draw()
 {
 
+	SDL_UpdateWindowSurface( window );
 
 }
 
-using namespace std;
+
+void close()
+{
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+    window = NULL;
+
+	SDL_Quit();
+}
+
 
 int main (int argc, char* argv[]){
 
 	initialize();
 
-	SDL_Window *window;
+	loadMedia();
 
-	SDL_Init(SDL_INIT_VIDEO);
+	update();
 
-	window = SDL_CreateWindow(
-		"An SDL2 window",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		640,
-		480,
-		SDL_WINDOW_OPENGL
-		);
+	draw();
 
+    SDL_Delay(3000);
 
-    // Check that the window was successfully made
-    if (window == NULL) {
-        // In the event that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
-
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-
-
-	//loadcontent
-	//unloadcontent
-	//-update
-	//-draw
-	//drawtext
-	//readwritefiles
-/*
-	while(true)
-	{
-		//Do Game
-
-		//Update
-		update();
-
-		//Draw
-		draw();
-	}
-*/
-	SDL_Quit();
+	close();
 	return 0;
 
 }
