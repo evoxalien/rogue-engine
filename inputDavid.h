89726ef9f640aa2d871060b5b761f94f1229b9b1
Dracolord1208 <dracolord1208@gmail.com>
2015-02-19 17:21:26 -0600

    Keyboard
    pretty sure all keyboard buttons are mapped. Still planning how to make
    the controls super fluid so you can do anything.

--- inputDavid.h
+++ inputDavid.h
@@ -30,14 +30,141 @@ void keyPress(GameState *gameState, SDL_Event *Event)
    //change the screen based on key 
    switch (Event->key.keysym.sym)
    {   
-      case SDLK_UP: *gameState=StartMenu;
+      case SDLK_UP:
       break;
-      case SDLK_DOWN: *gameState=EndMenu;
+      case SDLK_DOWN:
       break;
-      case SDLK_LEFT: *gameState=Playing;
+      case SDLK_LEFT:
       break;
-      case SDLK_RIGHT: *gameState=Paused;
+      case SDLK_RIGHT:
       break;
+      case SDLK_a:
+      break;
+      case SDLK_b:
+      break;
+      case SDLK_c:
+      break;
+      case SDLK_d:
+      break;
+      case SDLK_e:
+      break;
+      case SDLK_f:
+      break;
+      case SDLK_g:
+      break;
+      case SDLK_h:
+      break;
+      case SDLK_i:
+      break;
+      case SDLK_j:
+      break;
+      case SDLK_k:
+      break;
+      case SDLK_l:
+      break;
+      case SDLK_m:
+      break;
+      case SDLK_n:
+      break;
+      case SDLK_o:
+      break;
+      case SDLK_p:
+      break;
+      case SDLK_q:
+      break;
+      case SDLK_r:
+      break;
+      case SDLK_s:
+      break;
+      case SDLK_t:
+      break;
+      case SDLK_u:
+      break;
+      case SDLK_v:
+      break;
+      case SDLK_w:
+      break;
+      case SDLK_x:
+      break;
+      case SDLK_y:
+      break;
+      case SDLK_z:
+      break;
+      case SDLK_0:
+      break;
+      case SDLK_1:
+      break;
+      case SDLK_2:
+      break;
+      case SDLK_3:
+      break;
+      case SDLK_4:
+      break;
+      case SDLK_5:
+      break;
+      case SDLK_6:
+      break;
+      case SDLK_7:
+      break;
+      case SDLK_8:
+      break;
+      case SDLK_9:
+      break;
+      case SDLK_QUOTE:
+      break;
+      case SDLK_BACKSLASH:
+      break;
+      case SDLK_BACKSPACE:
+      break;
+      case SDLK_CAPSLOCK:
+      break;
+      case SDLK_DELETE:
+      break;
+      case SDLK_END:
+      break;
+      case SDLK_EQUALS:
+      break;
+      case SDLK_ESCAPE:
+      break;
+      case SDLK_HOME:
+      break;
+      case SDLK_INSERT:
+      break;
+      case SDLK_LALT:
+      break;
+      case SDLK_LCTRL:
+      break;
+      case SDLK_LEFTBRACKET:
+      break;
+      case SDLK_LSHIFT:
+      break;
+      case SDLK_MINUS:
+      break;
+      case SDLK_PAGEDOWN:
+      break;
+      case SDLK_PAGEUP:
+      break;
+      case SDLK_RALT:
+      break;
+      case SDLK_RCTRL:
+      break;
+      case SDLK_RETURN:
+      break;
+      case SDLK_RIGHTBRACKET:
+      break;
+      case SDLK_RSHIFT:
+      break;
+      case SDLK_SEMICOLON:
+      break;
+      case SDLK_SEPARATOR:
+      break;
+      case SDLK_SLASH:
+      break;
+      case SDLK_SPACE:
+      break;
+      case SDLK_TAB:
+      break;
+          
       default: *gameState=leveledup;
       break;
    } 
