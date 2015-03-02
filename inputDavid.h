4b9f0a05961c2aadd4edc44fa33074bb5b2fa42f
Dracolord1208 <dracolord1208@gmail.com>
2015-03-01 22:05:53 -0600

    sure
    mid setup

--- inputDavid.h
+++ inputDavid.h
@@ -24,150 +24,81 @@ enum GameState
 //keyboard david 
 //http://lazyfoo.net/tutorials/SDL/04_key_presses/index.php
 
-void keyPress(GameState *gameState, SDL_Event *Event)
+class basicInput
 {
-   //change the screen based on key 
-   switch (Event->key.keysym.sym)
-   {   
-      case SDLK_UP:
-      break;
-      case SDLK_DOWN:
-      break;
-      case SDLK_LEFT:
-      break;
-      case SDLK_RIGHT:
-      break;
-      case SDLK_a:
-      break;
-      case SDLK_b:
-      break;
-      case SDLK_c:
-      break;
-      case SDLK_d:
-      break;
-      case SDLK_e:
-      break;
-      case SDLK_f:
-      break;
-      case SDLK_g:
-      break;
-      case SDLK_h:
-      break;
-      case SDLK_i:
-      break;
-      case SDLK_j:
-      break;
-      case SDLK_k:
-      break;
-      case SDLK_l:
-      break;
-      case SDLK_m:
-      break;
-      case SDLK_n:
-      break;
-      case SDLK_o:
-      break;
-      case SDLK_p:
-      break;
-      case SDLK_q:
-      break;
-      case SDLK_r:
-      break;
-      case SDLK_s:
-      break;
-      case SDLK_t:
-      break;
-      case SDLK_u:
-      break;
-      case SDLK_v:
-      break;
-      case SDLK_w:
-      break;
-      case SDLK_x:
-      break;
-      case SDLK_y:
-      break;
-      case SDLK_z:
-      break;
-      case SDLK_0:
-      break;
-      case SDLK_1:
-      break;
-      case SDLK_2:
-      break;
-      case SDLK_3:
-      break;
-      case SDLK_4:
-      break;
-      case SDLK_5:
-      break;
-      case SDLK_6:
-      break;
-      case SDLK_7:
-      break;
-      case SDLK_8:
-      break;
-      case SDLK_9:
-      break;
-      case SDLK_QUOTE:
-      break;
-      case SDLK_BACKSLASH:
-      break;
-      case SDLK_BACKSPACE:
-      break;
-      case SDLK_CAPSLOCK:
-      break;
-      case SDLK_DELETE:
-      break;
-      case SDLK_END:
-      break;
-      case SDLK_EQUALS:
-      break;
-      case SDLK_ESCAPE:
-      break;
-      case SDLK_HOME:
-      break;
-      case SDLK_INSERT:
-      break;
-      case SDLK_LALT:
-      break;
-      case SDLK_LCTRL:
-      break;
-      case SDLK_LEFTBRACKET:
-      break;
-      case SDLK_LSHIFT:
-      break;
-      case SDLK_MINUS:
-      break;
-      case SDLK_PAGEDOWN:
-      break;
-      case SDLK_PAGEUP:
-      break;
-      case SDLK_RALT:
-      break;
-      case SDLK_RCTRL:
-      break;
-      case SDLK_RETURN:
-      break;
-      case SDLK_RIGHTBRACKET:
-      break;
-      case SDLK_RSHIFT:
-      break;
-      case SDLK_SEMICOLON:
-      break;
-      case SDLK_SEPARATOR:
-      break;
-      case SDLK_SLASH:
-      break;
-      case SDLK_SPACE:
-      break;
-      case SDLK_TAB:
-      break;
-          
-      default: *gameState=leveledup;
-      break;
-   } 
-   
-}
+private:
+   bool mouseArray[5];
+   int x, y;
+   SDL_Event event;
+
+   void getMouseButtonStates()
+   {
+      if(event.type == SDL_MOUSEBUTTONDOWN)
+      {
+         mouseArray[event.button.button - 1] = true;
+      }
+      if(event.type == SDL_MOUSEBUTTONUP)
+      {
+         mouseArray[event.button.button - 1] = false;
+      }
+   }
+public:
+   InputClass()
+   {
+      x = y = 0;
+      for(int i = 0; i < 5; i++)
+      {
+         mouseArray[i] = false;  
+      }
+   }
+
+   void update(SDL_Event e)
+   {
+      event = e;
+      SDL_GetMouseState(&x, &y);
+      getMouseButtonStates();
+   }
+
+   SDL_Keycode getKeyDown()
+   {
+      if(event.type == SDL_KEYDOWN)
+      {
+         return event.key.keysym.sym;
+      }
+      return -1;
+   }
+
+   SDL_Keycode getKeyUp()
+   {
+      if(event.type == SDL_KEYUP)
+      {
+         return event.key.keysym.sym;
+      }
+      return -1;
+   }
+
+   bool getMouseButton(int index)
+   {
+      if(index >= 0 && index <= 5)
+      {
+         return mouseArray[index - 1];
+      }
+      return false;
+   }
+
+   int getMouseX()
+   {
+      return x;
+   }
+
+   int getMouseY()
+   {
+      return y;
+   }
+};
+
+
+
+
 #endif
 
