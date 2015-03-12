65489dbf91e371ffeb5445c934050810640d19c3
Blargansmarf <joshpicarazzi@gmail.com>
2015-03-12 18:54:38 -0500

    Marcelo's Camera files
    I took Marcelo's camera implementation and changed it around a bit to
    make it more easily functioning with our current code base.

---
+++ Camera.cpp
@@ -0,0 +1,86 @@
+#include "Camera.h"
+
+//Constructor: 
+//Takes a bounding rectangle,
+//camera type either STATIC or NONSTATIC --> default is nonstatic
+Camera::Camera(SDL_Rect bRect, int iType)
+{
+	//Bounding Rectangles
+	BOUND_RECT = bRect;
+
+	if(iType != 0)
+	{
+		CAMERA_TYPE = "STATIC";
+	}else
+	{
+		CAMERA_TYPE = "NONSTATIC";
+	}
+
+
+	CINEMATIC = false;
+	camera = { 0, 0, CAMERA_WIDTH, CAMERA_HEIGHT};
+
+
+}//End of Constructor
+
+//Constructor: 
+//Takes Screen width and height, default is static
+Camera::Camera(int levelWidth, int levelHeight)
+{
+	CAMERA_TYPE = "STATIC";
+	BOUND_RECT = {0, 0, levelWidth, levelHeight};
+	camera = { 0, 0, CAMERA_WIDTH, CAMERA_HEIGHT};
+}
+
+Camera::~Camera()
+{
+
+}
+
+
+//Return Functions:
+//Different return function values
+int Camera::getCamX(){return camera.x;}
+int Camera::getCamY(){return camera.y;}
+int Camera::getCamW(){return camera.w;}
+int Camera::getCamH(){return camera.h;}
+
+//Bounderies:
+//Checks bounds for the camera not to overlap edge of level
+void Camera::CheckBounds()
+{
+	if( camera.x < BOUND_RECT.x )
+	{ 
+		camera.x = BOUND_RECT.x;
+	}
+	if( camera.y < BOUND_RECT.y )
+	{
+		camera.y = BOUND_RECT.y;
+	}
+
+	if( camera.x > BOUND_RECT.w - camera.w )
+	{
+		camera.x = BOUND_RECT.w - camera.w;
+	}
+	if( camera.y > BOUND_RECT.h - camera.h )
+	{
+		camera.y = BOUND_RECT.h - camera.h;
+	}
+
+}
+
+//Cinematic Mode:
+void Camera::Cinematic_Mode(bool bCinematic)
+{
+	CINEMATIC = bCinematic;
+}
+
+//Update Camera:
+//Updates camera's x and y to the integers passed in
+void Camera::Update_Camera(int camX, int camY)
+{
+	camera.x = camX;
+	camera.y = camY;
+	CheckBounds();
+
+}//End of Update
---
+++ Camera.h
@@ -0,0 +1,56 @@
+#ifndef CAMERA_H_
+#define CAMERA_H_
+#include "SDLincludes.h"
+#include <string>
+
+using namespace std;
+//Last updated: 3/12/15
+
+class Camera
+{
+private: 
+
+	//CAMERA BOUNDING AREA
+	SDL_Rect BOUND_RECT;
+
+	//CAMERA VIEWPORT
+	SDL_Rect camera;
+
+	//CAMERA CONSTANTS
+	const int CAMERA_WIDTH = 1282;
+	const int CAMERA_HEIGHT = 720;
+
+	//CAMERA TYPE
+	string CAMERA_TYPE;
+
+	//CINEMATIC
+	bool CINEMATIC;
+
+	//CHECK BOUNDS
+	void CheckBounds();
+
+public:
+	//CONSTRUCTOR
+	Camera(SDL_Rect bRect, int iType);
+	Camera(int levelHeight,int levelHeight);
+	//Camera();
+	~Camera();
+
+	//RETURN FUNCTIONS FOR VALUES
+	int getCamX();
+	int getCamY();
+	int getCamW();
+	int getCamH();
+
+	//SETTING THE CAMERA MODE EITHER FOR CINEMATICS
+	void Cinematic_Mode(bool);
+
+	//UPDATE THE CAMERA ON SCREEN
+	void Update_Camera(int camX,int camY);
+
+	//DRAW CAMERA TO SCREEN
+	//void Draw_Camera(SDL_Renderer* gRenderer);
+
+};
+
+#endif
