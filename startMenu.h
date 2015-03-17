79cba3f3112769f110dc7967941b9bd6bcb5e16d
Dracolord1208 <dracolord1208@gmail.com>
2015-03-17 18:41:42 -0500

    Menu
    testing menus

---
+++ startMenu.h
@@ -0,0 +1,11 @@
+#ifndef STARTMENU_H_INCLUDED
+
+#define STARTMENU_H_INCLUDED
+
+#include "SDLincludes.h"
+//#include "SDL.h"
+#include <stdio.h>
+#include <string>
+#include "inputDavid.h"
+#include "input.h"
+using namespace std;
