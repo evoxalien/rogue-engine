2337ecb53d496beec5d4cf40e3f9c3f874a1940b
Gigacrash <fake1021114@hotmail.com>
2015-03-23 03:38:14 -0500

    Semi-functional buttons
    Button now works. Offsets and centering need to be applied

--- button.h
+++ button.h
@@ -9,7 +9,8 @@ class button
 {
 private:
    SDL_Surface *tempSurface;
-   SDL_Window *windowCopy;
+   SDL_Surface *windowCopy;
+   TTF_Font *font;
 
 public:
    button(SDL_Window *windowOriganal);
@@ -20,34 +21,47 @@ public:
    void setX(int xIn);
    void setY(int yIn);
    void setText(string textIn);
+   void setColor(Uint8 rIn, Uint8 gIn, Uint8 bIn);
+   void setButtonColor(Uint8 rIn, Uint8 gIn, Uint8 bIn);
    bool draw();
    
    
    string text;
+   SDL_Color textColor;
+   SDL_Color buttonColor;
    SDL_Rect shape;
 
 };
 
 button::button(SDL_Window *windowOriganal)
 {
-   windowCopy = windowOriganal;
+   font = TTF_OpenFont( "lazy.ttf", 28);
+   windowCopy = SDL_GetWindowSurface(windowOriganal);
    text = "button";
    shape = { 0, 0, 5, 5};
+   textColor = { 255, 255, 255 };
+   buttonColor = { 255, 0, 0};
 }
 
 
 button::button(SDL_Window *windowCopy, string tempText)
 {
-   windowCopy = windowOriganal;
+   font = TTF_OpenFont( "lazy.ttf", 28);
+   windowCopy = SDL_GetWindowSurface(windowOriganal);
    text = tempText;
    shape = { 0, 0, 5, 5};
+   buttonColor = { 255, 0, 0};
+   
 }
 
 button::button(SDL_Window *windowCopy, string tempText,int xIn, int yIn, wIn, hIn)
 {
-   windowCopy = windowOriganal;
+   font = TTF_OpenFont( "lazy.ttf", 28);
+   windowCopy = SDL_GetWindowSurface(windowOriganal);
    text = tempText;
    shape = { xIn, yIn, wIn, hIn};
+   textColor = {255, 255, 255};
+   buttonColor = { 255, 0, 0};
 }
 
 void button::setWidth(int wIn)
@@ -75,9 +89,23 @@ void button::setText(string textIn)
    text = textIn;
 }
 
+void button::setTextColor(Uint8 rIn, Uint8 gIn, Uint8 bIn)
+{
+   textColor = {rIn, gIn, bIn};
+}
+
+void button::setButtonColor(Uint8 rIn, Uint8 gIn, Uint8 bIn)
+{
+   buttonColor = {rIn, gIn, bIn};
+}
+
 bool button::draw()
 {
    //working with font library for most complicated part
+   //currently text does not always fit in button
+   tempSurface = TTF_RenderText_Solid(font, text, textColor);
+   SDL_FillRect(windowCopy, shape, buttonColor);
+   apply_surface( shape.x, shape.y, tempSurface, windowCopy);
 }
 
 #endif
